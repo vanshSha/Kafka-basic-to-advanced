401d94c11e117ba85ca467aa992d43e8317f9878
Vansh Sharma <vansh@Vanshs-MacBook-Air.local>
2025-08-23 02:05:58 +0530

    Initial commit of kafka-REST-Proxy project

---
+++ kafka-REST-Proxy/src/main/java/rest_proxy/KafkaRestProxyApplication.java
@@ -0,0 +1,13 @@
+package rest_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestProxyApplication.class, args);
+	}
+
+}
---
+++ kafka-REST-Proxy/src/test/java/rest_proxy/KafkaRestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package rest_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRestProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
