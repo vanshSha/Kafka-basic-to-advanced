124d2687302b8c53b857951b5d3e67335805b066
Vansh Sharma <vansh@Vanshs-MacBook-Air.local>
2025-09-01 12:04:52 +0530

    Updated configs and source files

--- kafka-connect-sample/src/main/java/kafka_connect/KafkaConnectSampleApplication.java
+++ kafka-connect-sample/src/main/java/kafka_connect/KafkaConnectSampleApplication.java
@@ -13,19 +13,20 @@ import java.io.File;
 
 @SpringBootApplication
 //@EnableKafkaStreams
-public class KafkaConnectSampleApplication implements CommandLineRunner {
+// implements CommandLineRunner
+public class KafkaConnectSampleApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaConnectSampleApplication.class, args);
     }
 
-    @Autowired
-    private BinaryAsBased64Producer producer;
-
-    @Override
-    public void run(String... args) throws Exception {
-		File file = new File("src/main/resources/RK098_5.jpg.webp");
-
-        producer.send(KafkaConfig.TOPIC_DEMO_BINARY, file);
-    }
+//    @Autowired
+//    private BinaryAsBased64Producer producer;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//		File file = new File("src/main/resources/RK098_5.jpg.webp");
+//
+//        producer.send(KafkaConfig.TOPIC_DEMO_BINARY, file);
+//    }
 }
